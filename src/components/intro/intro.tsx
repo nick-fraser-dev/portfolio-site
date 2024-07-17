23f3eebe433a149bac19e8109b21da967f2f049e
3b3n3z3r666 <nickfraser40@gmail.com>
2024-07-16 21:39:19 -0400

    fix smartphone nav-bar bug

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,7 +4,7 @@ import IntroAbout from './intro-about';
 import IntroLinks from './intro-links';
 import { useSectionInView } from '@/lib/hooks';
 export default function Intro() {
-  const {ref } = useSectionInView("Home");
+  const {ref } = useSectionInView("Home", 0.5);
   return (
     
     <section className='mb-0 max-w-[50rem] text-center sm:mb-2 sm:mt-12 scroll-mt-48' id="home" ref={ref}>
