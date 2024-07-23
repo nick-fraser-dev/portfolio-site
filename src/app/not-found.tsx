4ec46e86ec682a4975a924960cc27aa3140b108a
3b3n3z3r666 <nickfraser40@gmail.com>
2024-07-23 15:38:52 -0400

    fix home button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,12 +10,11 @@ export default function NotFound() {
     return (
         <div className="flex flex-col font-light tracking-widest text-3xl items-center text-center gap-4">
             {pathname} not found.
-            <div className="flex flex-row  items-center text-center justify-center align-middle bg-black text-white text-xl rounded-lg gap-2 shadow-xl px-4 py-2 hover:scale-110 group transition">
-                <Link href="/" className="px-2" >Return Home </Link>
-                <BiArrowBack className="group-hover:-translate-x-4 transition" />
-            </div>
-
-
+            
+                <Link href="/" className="flex flex-row  items-center text-center justify-center align-middle bg-black text-white text-xl rounded-lg gap-2 shadow-xl px-4 py-2 hover:scale-110 group transition cursor-pointer" >
+                Return Home 
+                <BiArrowBack className="group-hover:-translate-x-3 transition" />
+                </Link>
         </div>
 
     )
